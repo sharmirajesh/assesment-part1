a602fe0e28a1b19ba8e1d2c135a1704bf406fa22
sharmirajesh <48148551+sharmirajesh@users.noreply.github.com>
2019-08-09 14:30:26 +0530

    Create five node.py

---
+++ five node.py
@@ -0,0 +1,10 @@
+class node:
+    def __init__(self,x):
+        self.data=x
+        self.add=None
+n1=node(500)
+n2=node(100)
+n3=node(200)
+n4=node(300)
+n5=node(400)
+print(n1.data,n2.data,n3.data,n4.data,n5.data)
